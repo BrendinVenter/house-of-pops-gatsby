cf1eec0f7b06378bd5e47dab2fdf98f5f1504c72
Brendin Venter <brendincolinventer@gmail.com>
2021-09-06 12:07:42 +0400

    Added gatsby graphQL layer

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['warn', { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    indent: [
+      'error',
+      2,
+      {
+        ignoredNodes: [
+          'JSXElement',
+          'JSXElement > *',
+          'JSXAttribute',
+          'JSXIdentifier',
+          'JSXNamespacedName',
+          'JSXMemberExpression',
+          'JSXSpreadAttribute',
+          'JSXExpressionContainer',
+          'JSXOpeningElement',
+          'JSXClosingElement',
+          'JSXText',
+          'JSXEmptyExpression',
+          'JSXSpreadChild',
+        ],
+      },
+    ],
+    'comma-dangle': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 0,
+    'implicit-arrow-linebreak': 'off',
+    'operator-linebreak': 'off',
+    'import/prefer-default-export': 'off',
+    'object-curly-newline': 'off',
+  },
+};
--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   &:hover {
-    background-color: dodgerblue;
+    background-color: ${({ secondary }) =>
+      secondary ? 'var(--yellow)' : 'var(--blue)}'};
+  }
+
+  &:disabled {
+    background-color: var(--grey);
   }
 `;
 
